12ed4328b7f614a5d35810e77064758f4450d2ae
zaynb <zeinabragab6@gmail.com>
2022-04-03 20:28:15 +0200

    add new testcase (block user)
    cx_automation

--- CX-automation-testing/tests/loginTest.js
+++ CX-automation-testing/tests/loginTest.js
@@ -8,6 +8,7 @@ const homeObj     = new homePage()
 const CxUrl      = 'http://87.119.195.19/emasraf-cx/#/Login'
 
 const username        = testdata.validCxUsername;
+const username2       = testdata.validCxUsername2;
 const password        = testdata.validCxPassword;
 const invalidUsername = testdata.invalidAvlUsername ;
 const invalidPassword = testdata.invalidAvlPassword ;
@@ -33,14 +34,29 @@ test
 
 
 test
-.meta({ID: 'TEST-002', SEVERITY: 'blocker', USER_STORY: '002',TEST_RUN: '001'})
+.meta({ID: 'TEST-002', SEVERITY: 'blocker', USER_STORY: '002',TEST_RUN: '002'})
 ('valid login', async t =>{
     loginObj.enterUsername(username)
     loginObj.enterPassword(password)
     loginObj.pressLogin(t)
     await t.expect(loginMenuBtnAssertion.visible,{timeout: 5000}).ok()
+}) 
+
+test
+.meta({ID: 'TEST-003', SEVERITY: 'blocker', USER_STORY: '003',TEST_RUN: '003'})
+('block user after 2 invalid trial ', async t =>{
+    loginObj.enterUsername(username2)
+    loginObj.enterPassword(invalidPassword)
+    loginObj.pressLogin(t)
+
+    loginObj.enterUsername(username2)
+    loginObj.enterPassword(invalidPassword)
+    loginObj.pressLogin(t)
+
+    loginObj.enterUsername(username2)
+    loginObj.enterPassword(password)
+    loginObj.pressLogin(t)
+    await t.expect(loginErrorMsg.visible,{timeout: 5000}).ok();    
 })
-
-
 
 
